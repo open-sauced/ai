48d3e6ae5d5441c2779db3b413fa374dfb2e33c5
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-04-17 22:53:18 +0530

    remove tailwind normalising classes

--- src/components/ViewOnOpenSaucedButton/ViewOnOpenSaucedButton.ts
+++ src/components/ViewOnOpenSaucedButton/ViewOnOpenSaucedButton.ts
@@ -17,12 +17,12 @@ function injectViewOnOpenSaucedButton() {
   const viewOnOpenSaucedButton = document.createElement("a");
   viewOnOpenSaucedButton.href = `https://insights.opensauced.pizza/user/${username}/contributions`;
   viewOnOpenSaucedButton.className =
-    "inline-block mt-4 mb-1 text-white rounded-md p-2 no-underline text-md font-semibold text-center select-none w-full border cursor-pointer border-orange hover:shadow-button";
+    "inline-block mt-4 mb-1 text-white rounded-md p-2 no-underline text-md font-semibold text-center select-none w-full border border-solid cursor-pointer border-orange hover:shadow-button hover:no-underline";
   viewOnOpenSaucedButton.target = "_blank";
   viewOnOpenSaucedButton.rel = "noopener noreferrer";
   viewOnOpenSaucedButton.innerHTML = `
     <img
-      class="mx-2 inline-block"
+      class="mx-2 inline-block align-top"
       src="${chrome.runtime.getURL(
         logoIcon
       )}"
