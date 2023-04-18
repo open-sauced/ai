8269c6f8540b68f1591ef8e15c4a4aaebb667f0c
Anush008 <anushshetty90@gmail.com>
2023-04-18 21:57:25 +0530

    refactor: Moved injectViewOnOpenSauced to utils

---
+++ src/utils/view-on-OS.ts
@@ -0,0 +1,13 @@
+//import {USER_BIO_SELECTOR} from "../config.json" assert { type: 'json' };
+import { ViewOnOpenSaucedButton } from "../components/ViewOnOpenSaucedButton/ViewOnOpenSaucedButton";
+
+const injectViewOnOpenSauced = (username: string) => {
+    const viewOnOpenSaucedButton = ViewOnOpenSaucedButton(username);
+
+    const userBio = document.querySelector(".p-note.user-profile-bio");
+    if (!userBio) return;
+    
+    userBio.appendChild(viewOnOpenSaucedButton);
+}
+
+export default injectViewOnOpenSauced;
