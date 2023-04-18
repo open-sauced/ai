a83345270a31811fff74cd6aa747df70b6c2d41a
Anush008 <anushshetty90@gmail.com>
2023-04-18 21:56:18 +0530

    chore: Updated getOpenSaucedUser() to return a bool

--- src/utils/fetchOpenSaucedApiData.ts
+++ src/utils/fetchOpenSaucedApiData.ts
@@ -1,9 +1,8 @@
-export const getOpenSaucedUser = async (username: string) => {
-  const response = await fetch(
+export const getOpenSaucedUser = (username: string) => {
+  fetch(
     `https://api.opensauced.pizza/v1/users/${username}`
-  );
-  if (response.status !== 200) {
-    return null;
-  }
-  return await response.json();
+  ).then(response => {
+    if (response.status !== 200) return false;
+  });
+  return true;
 };
