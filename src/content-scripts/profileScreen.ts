67efb60599e7bbe2d80eb3eef39db611f54078ac
Anush008 <anushshetty90@gmail.com>
2023-04-19 17:52:13 +0530

    chore: Top level await for getOpenSaucedUser()

--- src/content-scripts/profileScreen.ts
+++ src/content-scripts/profileScreen.ts
@@ -1,11 +1,11 @@
-import { getGithubUsername } from "../utils/matchers/gh-username-matcher";
+import { getGithubUsername } from "../utils/matchers";
 import { getOpenSaucedUser } from "../utils/fetchOpenSaucedApiData";
-import injectViewOnOpenSauced from "../utils/view-on-OS";
-import injectInviteToOpenSauced from "../utils/invite-to-OS";
+import injectViewOnOpenSauced from "../utils/viewOnOpenSauced";
+import injectInviteToOpenSauced from "../utils/inviteToOpenSauced";
 
 const username = getGithubUsername(window.location.href);
 if (username != null) {
-  const openSaucedUser = getOpenSaucedUser(username);
+  const openSaucedUser = await getOpenSaucedUser(username);
   if (openSaucedUser) injectViewOnOpenSauced(username);
   else injectInviteToOpenSauced(username);
 }
