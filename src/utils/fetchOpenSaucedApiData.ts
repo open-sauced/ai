c9fef9068be49fdf70f6fe0af2b8c0b3a87d5c6a
Anush <anushshetty90@gmail.com>
2023-04-19 08:15:49 +0530

    chore: Removed explicit book return and err handling

--- src/utils/fetchOpenSaucedApiData.ts
+++ src/utils/fetchOpenSaucedApiData.ts
@@ -1,8 +1,17 @@
-export const getOpenSaucedUser = (username: string) => {
-  fetch(`https://api.opensauced.pizza/v1/users/${username}`).then(
-    (response) => {
-      if (response.status !== 200) return false;
-    }
-  );
-  return true;
-};
+export const getOpenSaucedUser = async (username) => {
+    try {
+        const response = await fetch(`https://api.opensauced.pizza/v1/users/${username}`);
+        return response.status === 200;
+    } catch (error) {
+        return false;
+    }    
+    };
+
+
+    
+
+
+       
+
+
+
