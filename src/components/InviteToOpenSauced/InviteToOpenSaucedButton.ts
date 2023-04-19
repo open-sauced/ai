40a45ee32d978f9709da99d048d4ae40261314b6
Anush008 <anushshetty90@gmail.com>
2023-04-19 19:46:21 +0530

    chore: Updated elements to use the new createHtmlElement()

--- src/components/InviteToOpenSauced/InviteToOpenSaucedButton.ts
+++ src/components/InviteToOpenSauced/InviteToOpenSaucedButton.ts
@@ -1,13 +1,12 @@
 import logoIcon from "../../assets/opensauced-icon.svg";
 import "../../index.css";
+import { createHtmlElement } from "../../utils/createHtmlElement";
 
 export const InviteToOpenSaucedButton = () => {
-  const inviteToOpenSaucedButton = document.createElement("a");
-  Object.assign(inviteToOpenSaucedButton, {
+  const inviteToOpenSaucedButton = createHtmlElement("a", {
     className:
       "inline-block mt-4 mb-1 text-white rounded-md p-2 no-underline text-md font-semibold text-center select-none w-full border border-solid cursor-pointer border-orange hover:shadow-button hover:no-underline",
-    innerHTML: `
-        <img
+    innerHTML: `<img
           class="mx-2 inline-block align-top"
           src="${chrome.runtime.getURL(logoIcon)}"
           alt="OpenSauced Logo"
--- src/components/InviteToOpenSauced/InviteToOpenSaucedModal.ts
+++ src/components/InviteToOpenSauced/InviteToOpenSaucedModal.ts
@@ -1,4 +1,8 @@
 import "../../index.css";
+import { createHtmlElement } from "../../utils/createHtmlElement";
+import emailSocialIcon from "../../assets/mail-icon.svg";
+import twitterSocialIcon from "../../assets/twitter-icon.svg";
+import linkedInSocailIcon from "../../assets/linkedin-icon.svg";
 
 interface Socials {
   emailAddress?: string;
@@ -10,37 +14,46 @@ export const InviteToOpenSaucedModal = (
   username: string,
   { emailAddress, twitterUsername, linkedInUsername }: Socials = {}
 ) => {
-  //TODO: Conditionally render social icons
-  const socialIcons = `<span class="flex flex-nowrap space-x-3"> <a href="https://github.com/bdougie">
-    <img src="https://images.weserv.nl/?url=cdn4.iconfinder.com/data/icons/miu-flat-social/60/mail-512.png&h=60&w=60&fit=cover&mask=circle" alt="Email">
-  </a>
-  <a href="https://github.com/bdougie">
-  <img src="https://images.weserv.nl/?url=cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png&h=60&w=60&fit=cover&mask=circle" alt="Twitter">
-</a>
-<a href="https://github.com/bdougie">
-<img src="https://images.weserv.nl/?url=cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png&h=60&w=60&fit=cover&mask=circle" alt="LinkedIn">
-</a>
-</span>`;
+  const emailIcon = emailAddress ? createHtmlElement("a", {
+    href: `mailto:${emailAddress}`,
+    innerHTML: `<img src=${chrome.runtime.getURL(emailSocialIcon)} alt="Email">`,
+  }) : "";
+  const twitterIcon = twitterUsername ? createHtmlElement("a", {
+    href: `https://twitter.com/${twitterUsername}`,
+    innerHTML: `<img src=${chrome.runtime.getURL(twitterSocialIcon)} alt="Twitter">`,
+  }) : "";
+  const linkedInIcon = linkedInUsername ? createHtmlElement("a", {
+    href: `https://www.linkedin.com/in/${linkedInUsername}`,
+    innerHTML: `<img src=${chrome.runtime.getURL(linkedInSocailIcon)} alt="LinkedIn">`,
+  }) : "";
 
-  const inviteToOpenSaucedModal = document.createElement("div");
-  Object.assign(inviteToOpenSaucedModal, {
-    className:
-      "fixed h-full w-full z-50 bg-gray-600 bg-opacity-50 overflow-y-auto  inset-0 ",
-    style: "display: none;",
-    id: "invite-modal",
-    innerHTML: `<div
-        class="min-w-[30%] relative top-60 mx-auto p-4 border w-96 rounded-md shadow-button border-solid border-orange bg-slate-800"
-    >
-        <div class="mt-2 text-left">
-            <h3 class="text-2xl leading-6 font-bold">Invite to OpenSauced!</h3>
-            <div class="mt-2">
-                <p class="text-sm">
-                    Use the social icons below to invite ${username} to OpenSauced.
-                </p>
-            </div>
-            ${socialIcons}
-        </div>
-    </div>`,
+  const socialIcons = createHtmlElement("span", {
+    className: "flex flex-nowrap space-x-3",
   });
+
+  const inviteToOpenSaucedModal = createHtmlElement("div", {
+    className:
+      "fixed h-full w-full z-50 bg-gray-600 bg-opacity-50 overflow-y-auto inset-0",
+    style: { display: "none" },
+    id: "invite-modal",
+  });
+
+  const inviteToOpenSaucedModalContainer = createHtmlElement("div", {
+    className:
+      "mt-2 text-left min-w-[30%] relative top-60 mx-auto p-4 border w-96 rounded-md shadow-button border-solid border-orange bg-slate-800",
+    innerHTML: `
+    <h3 class="text-2xl leading-6 font-bold">Invite ${username} to OpenSauced!</h3>
+    <div class="mt-2">
+       <p class="text-sm">
+          Use the social icons below to invite them to OpenSauced.
+       </p>
+    </div>
+`,
+  });
+
+  socialIcons.replaceChildren(emailIcon, twitterIcon, linkedInIcon);
+  inviteToOpenSaucedModalContainer.appendChild(socialIcons);
+  inviteToOpenSaucedModal.appendChild(inviteToOpenSaucedModalContainer);
+
   return inviteToOpenSaucedModal;
 };
