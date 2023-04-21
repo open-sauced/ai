357aab514ee1cbcd0e61c3220cff20c7c8a197be
Anush <anushshetty90@gmail.com>
2023-04-21 20:16:46 +0530

    fix: Wrapped the await call in an async() (#26)

--- src/content-scripts/profileScreen.ts
+++ src/content-scripts/profileScreen.ts
@@ -3,9 +3,12 @@ import { getOpenSaucedUser } from "../utils/fetchOpenSaucedApiData";
 import injectViewOnOpenSauced from "../utils/dom-utils/viewOnOpenSauced";
 import injectInviteToOpenSauced from "../utils/dom-utils/inviteToOpenSauced";
 
+const processProfilePage = async () => {
 const username = getGithubUsername(window.location.href);
 if (username != null) {
   const openSaucedUser = await getOpenSaucedUser(username);
   if (openSaucedUser) injectViewOnOpenSauced(username);
   else injectInviteToOpenSauced(username);
 }
+};
+processProfilePage();
