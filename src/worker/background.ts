4aa584c7992c4c21d89c4ca98c6283d13cc905a5
Anush008 <anushshetty90@gmail.com>
2023-04-26 20:24:01 +0530

    feat: gh-page-update-listener

--- src/worker/background.ts
+++ src/worker/background.ts
@@ -1,8 +1,14 @@
-import  { SUPABASE_LOGOUT_URL, OPEN_SAUCED_AUTH_TOKEN_KEY } from "../constants";
+import { SUPABASE_LOGOUT_URL, OPEN_SAUCED_AUTH_TOKEN_KEY } from "../constants";
 
 chrome.webRequest.onCompleted.addListener(
-    (details) => {
-      chrome.storage.sync.remove(OPEN_SAUCED_AUTH_TOKEN_KEY);
-    },
-    { urls: [SUPABASE_LOGOUT_URL] }
-  );
+  (details) => {
+    chrome.storage.sync.remove(OPEN_SAUCED_AUTH_TOKEN_KEY);
+  },
+  { urls: [SUPABASE_LOGOUT_URL] }
+);
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+  if (changeInfo.url?.includes("github.com")) {
+    chrome.tabs.sendMessage(tabId, { message: "GITHUB_URL_CHANGED" });
+  }
+});
