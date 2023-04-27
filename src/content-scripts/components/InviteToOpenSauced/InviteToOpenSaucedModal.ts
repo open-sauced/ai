6063bf18e67217348acb5fb5817a9b47db97d2ab
Anush008 <anushshetty90@gmail.com>
2023-04-27 18:36:15 +0530

    chore: Updated opacity and text color of invite modal of light mode

--- src/content-scripts/components/InviteToOpenSauced/InviteToOpenSaucedModal.ts
+++ src/content-scripts/components/InviteToOpenSauced/InviteToOpenSaucedModal.ts
@@ -68,7 +68,7 @@ export const InviteToOpenSaucedModal = (
 
   const inviteToOpenSaucedModal = createHtmlElement("div", {
     className:
-      "fixed h-full w-full z-50 bg-gray-600 bg-opacity-50 overflow-y-auto inset-0",
+      "fixed h-full w-full z-50 bg-gray-600 bg-opacity-80 dark:bg-opacity-50 overflow-y-auto inset-0",
     style: { display: "none" },
     id: "invite-modal",
   });
@@ -77,9 +77,9 @@ export const InviteToOpenSaucedModal = (
     className:
       "mt-2 min-w-[33%] relative top-60 mx-auto p-4 border w-96 rounded-md shadow-button border-solid border-orange bg-slate-800",
     innerHTML: `
-    <h3 class="text-2xl leading-6 font-bold">Invite ${username} to <a href="https://insights.opensauced.pizza/start"><span class="hover:text-orange hover:underline">OpenSauced!</span></a></h3>
+    <h3 class="text-2xl leading-6 font-bold text-white">Invite ${username} to <a href="https://insights.opensauced.pizza/start"><span class="hover:text-orange hover:underline">OpenSauced!</span></a></h3>
     <div class="mt-2">
-       <p class="text-md">
+       <p class="text-md text-white">
           Use the links below to invite them.
        </p>
     </div>
