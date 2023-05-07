0589d54a4e3f4a8e39f6a5116c900932db89cc8d
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-05-07 05:38:56 +0530

    final ui/ux changes

--- src/content-scripts/components/RepoVoting/RepoVoteButton.ts
+++ src/content-scripts/components/RepoVoting/RepoVoteButton.ts
@@ -2,18 +2,22 @@ import "../../content-scripts.css";
 import { createHtmlElement } from "../../../utils/createHtmlElement";
 import { getAuthToken } from "../../../utils/checkAuthentication";
 import { OPEN_SAUCED_REPOS_ENDPOINT } from "../../../constants";
+import { RepoUnvoteButton } from "./RepoUnvoteButton";
 
 export const VoteRepoButton = (ownerName: string, repoName: string) => {
   const voteRepoButton = createHtmlElement("a", {
     className:
-      "inline-block mt-4 text-black bg-gh-white dark:bg-gh-gray dark:text-white rounded-md p-2 text-sm font-semibold text-center select-none border hover:shadow-button hover:no-underline",
+      "inline-block text-white rounded-md  text-sm  text-center select-none border hover:shadow-button hover:no-underline no-underline border-none cursor-pointer bg-gradient-to-r from-[#e67e22] to-[#d35400] py-1 px-2 mx-2",
     innerHTML: `
             <span>Upvote</span>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"></path></g></svg>
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" class="align-middle"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"></path></g></svg>
         `,
   });
 
   voteRepoButton.addEventListener("click", async () => {
+    voteRepoButton.innerHTML = `
+      <span>Loading...</span>
+    `;
     const userToken = await getAuthToken();
 
     const response = await fetch(
@@ -25,8 +29,9 @@ export const VoteRepoButton = (ownerName: string, repoName: string) => {
     );
 
     if (response.status === 200) {
-      // TODO: Update the UI to reflect the vote
-      console.log("Voted");
+      const unvoteRepoButton = RepoUnvoteButton(ownerName, repoName);
+
+      voteRepoButton.replaceWith(unvoteRepoButton);
     } else {
       console.log("Something went wrong");
     }
