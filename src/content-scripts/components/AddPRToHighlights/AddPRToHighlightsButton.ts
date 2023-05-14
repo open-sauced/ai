ce45bb7d044c7a0f4cad9661a2e7d027336763f1
Anush008 <anushshetty90@gmail.com>
2023-05-14 23:59:19 +0530

    feat: pr actions dropdown

--- src/content-scripts/components/AddPRToHighlights/AddPRToHighlightsButton.ts
+++ src/content-scripts/components/AddPRToHighlights/AddPRToHighlightsButton.ts
@@ -3,13 +3,17 @@ import openSaucedLogoIcon from "../../../assets/opensauced-icon.svg";
 
 export const AddPRToHighlightsButton = () => {
   const addPRToHighlightsButton = createHtmlElement("a", {
-    href: `https://insights.opensauced.pizza/feed?url=${encodeURIComponent(window.location.href)}`,
-    target: "_blank",
-    rel: "noopener noreferrer",
-    innerHTML: `<span aria-label="Add PR to OpenSauced highlights." data-view-component="true" class="tooltipped tooltipped-n">
-    <img data-view-component="true" class="mr-1 mt-1" height="16px" width="16px" src=${chrome.runtime.getURL(openSaucedLogoIcon)}>
-    </span>`,
+    className: "relative",
+    innerHTML: `<span aria-label="Add PR to OpenSauced highlights" class="tooltipped tooltipped-n">
+    <img class="mr-1 mt-1" height="16px" width="16px" src=${chrome.runtime.getURL(
+      openSaucedLogoIcon
+    )}>
+    </span>
+    <details-menu class="dropdown-menu dropdown-menu-sw color-fg-default w-48 mt-2">
+    <a href="https://insights.opensauced.pizza/feed?prurl=${encodeURIComponent(window.location.href)}" class="dropdown-item" target="_blank">
+      + Add PR to Highlights
+    </a>
+  </details-menu>`
   });
-
   return addPRToHighlightsButton;
-};
+}
