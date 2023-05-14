da4a1de8b90c833f86714d02085aebaa0a9435b4
Anush <anushshetty90@gmail.com>
2023-05-14 17:22:59 +0530

    Update profile.tsx

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -152,7 +152,7 @@ export const Profile = () => {
           </div>
 
           <div className="flex flex-col items-center justify-center p-2 text-xs">
-            <p>Open PRs</p>
+            <p>PR Count</p>
 
             <p className="font-medium text-5xl">
               {userPR?.meta.itemCount}
