97a21a6a7a2209045deb1edd03d3604cb9d1f853
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-23 22:08:25 +0530

    change icons in the home page

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,8 +1,12 @@
 import { FaChevronLeft } from "react-icons/fa";
 import OpenSaucedLogo from "../assets/opensauced-logo.svg";
 import { EXTERNAL_RESOURCES } from "../constants";
-import { HiArrowTopRightOnSquare } from "react-icons/hi2";
+import {
+  HiOutlineBookOpen,
+  HiOutlineChatBubbleLeftRight,
+} from "react-icons/hi2";
 import { goBack } from "react-chrome-extension-router";
+import { VscIssues } from "react-icons/vsc";
 
 const Help = () => (
   <div className="p-4 bg-slate-800">
@@ -27,12 +31,10 @@ const Help = () => (
       </header>
 
       <main className="main-content text-white">
-        <h3 className="text font-medium text-base leading-10">
-          Help:
-        </h3>
+        <h3 className="text font-medium text-base leading-10">Help:</h3>
 
         <div className="tools flex flex-col gap-2">
-          {EXTERNAL_RESOURCES.map(externalLink => (
+          {EXTERNAL_RESOURCES.map((externalLink) => (
             <a
               key={externalLink.key}
               className="flex items-center bg-slate-700 hover:bg-slate-700/70 text-white hover:text-orange no-underline gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
@@ -40,7 +42,13 @@ const Help = () => (
               rel="noreferrer"
               target="_blank"
             >
-              <HiArrowTopRightOnSquare />
+              {externalLink.key === "Docs" && <HiOutlineBookOpen />}
+
+              {externalLink.key === "Issues" && <VscIssues />}
+
+              {externalLink.key === "Discussions" && (
+                <HiOutlineChatBubbleLeftRight />
+              )}
 
               {externalLink.key}
             </a>
