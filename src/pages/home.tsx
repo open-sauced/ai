a9882cf427d5a86d154456bc3f0349dfc31fdd0d
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-23 21:44:56 +0530

    change the icon for help page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import {
   HiArrowTopRightOnSquare,
+  HiOutlineQuestionMarkCircle,
   HiPencil,
-  HiSquare3Stack3D,
   HiUserCircle,
 } from "react-icons/hi2";
 import OpenSaucedLogo from "../assets/opensauced-logo.svg";
@@ -17,85 +17,77 @@ const Home = () => {
   const { currentTabIsOpensaucedUser, checkedUser } = useOpensaucedUserCheck();
 
   return (
-  <div className="p-4 bg-slate-800">
-    <div className="grid grid-cols-1 divide-y divide-white/40 divider-y-center-2 min-w-[320px] text-white">
-      <header className="flex justify-between">
-        <img
-          alt="OpenSauced logo"
-          className="w-[45%]"
-          src={OpenSaucedLogo}
-        />
+    <div className="p-4 bg-slate-800">
+      <div className="grid grid-cols-1 divide-y divide-white/40 divider-y-center-2 min-w-[320px] text-white">
+        <header className="flex justify-between">
+          <img alt="OpenSauced logo" className="w-[45%]" src={OpenSaucedLogo} />
 
-        {user && (
-          <button
-            className="flex gap-1 items-center hover:text-orange text-white no-underline"
-            onClick={() => {
-              goTo(Profile, { username: user.user_name });
-            }}
-          >
-            {user.user_name}
+          {user && (
+            <button
+              className="flex gap-1 items-center hover:text-orange text-white no-underline"
+              onClick={() => {
+                goTo(Profile, { username: user.user_name });
+              }}
+            >
+              {user.user_name}
 
-            <img
-              alt="User avatar"
-              className="rounded-full w-6 aspect-square border border-orange"
-              src={`https://github.com/${user.user_name}.png`}
-            />
-          </button>
-        )}
-      </header>
+              <img
+                alt="User avatar"
+                className="rounded-full w-6 aspect-square border border-orange"
+                src={`https://github.com/${user.user_name}.png`}
+              />
+            </button>
+          )}
+        </header>
 
-      <main className="main-content">
-        <h3 className="text font-medium text-base leading-10">Tools:</h3>
+        <main className="main-content">
+          <h3 className="text font-medium text-base leading-10">Tools:</h3>
 
-        <div className="tools flex flex-col gap-2">
-          <a
-            className="flex items-center bg-slate-700 hover:bg-slate-700/70 text-white hover:text-orange no-underline gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
-            href="https://insights.opensauced.pizza/feed"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <HiArrowTopRightOnSquare />
-            Go to Highlights feed
-          </a>
+          <div className="tools flex flex-col gap-2">
+            <a
+              className="flex items-center bg-slate-700 hover:bg-slate-700/70 text-white hover:text-orange no-underline gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
+              href="https://insights.opensauced.pizza/feed"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <HiArrowTopRightOnSquare />
+              Go to Highlights feed
+            </a>
 
-          <a
-            className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
-            href="https://insights.opensauced.pizza"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <HiArrowTopRightOnSquare />
-            Go to Dashboard
-          </a>
+            <a
+              className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
+              href="https://insights.opensauced.pizza"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <HiArrowTopRightOnSquare />
+              Go to Dashboard
+            </a>
 
-          <button
-            className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
-            onClick={() => {
-              goTo(AIPRDescription);
-            }}
-          >
-            <HiPencil />
-            AI PR Description
-          </button>
-
-          {currentTabIsOpensaucedUser && (
             <button
               className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
               onClick={() => {
-                goTo(Profile, { username: checkedUser });
+                goTo(AIPRDescription);
               }}
             >
-              <HiUserCircle />
-              View
-
-{" "}
-
-{checkedUser}
-              &apos;s profile
+              <HiPencil />
+              AI PR Description
             </button>
-          )}
-        </div>
-      </main>
+
+            {currentTabIsOpensaucedUser && (
+              <button
+                className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
+                onClick={() => {
+                  goTo(Profile, { username: checkedUser });
+                }}
+              >
+                <HiUserCircle />
+                View {checkedUser}
+                &apos;s profile
+              </button>
+            )}
+          </div>
+        </main>
 
         <footer className="tools flex gap-2">
           <button
@@ -104,12 +96,12 @@ const Home = () => {
               goTo(Help);
             }}
           >
-            <HiSquare3Stack3D />
+            <HiOutlineQuestionMarkCircle />
             Help
           </button>
         </footer>
+      </div>
     </div>
-  </div>
   );
 };
 
