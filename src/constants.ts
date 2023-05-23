5a54b74dbaabf78d7a25a65f3ad2dd59101f498f
Anush <anushshetty90@gmail.com>
2023-05-23 18:20:57 +0530

    refactor: removed author check from addToHighlights

--- src/constants.ts
+++ src/constants.ts
@@ -15,8 +15,6 @@ export const OPEN_SAUCED_AI_PR_DESCRIPTION_ENDPOINT = "https://api.opensauced.pi
 // GitHub constants/selectors
 export const GITHUB_PROFILE_MENU_SELECTOR = ".p-nickname.vcard-username.d-block";
 export const GITHUB_PROFILE_EDIT_MENU_SELECTOR = "button.js-profile-editable-edit-button";
-export const GITHUB_PR_AUTHOR_USERNAME_SELECTOR = "author Link--primary text-bold css-overflow-wrap-anywhere";
-export const GITHUB_LOGGED_IN_USER_USERNAME_SELECTOR = "meta[name=\"user-login\"]";
 export const GITHUB_PR_COMMENT_HEADER_SELECTOR = "timeline-comment-header clearfix d-flex";
 export const GITHUB_NEW_PR_COMMENT_EDITOR_SELECTOR = "flex-nowrap d-none d-md-inline-block mr-md-0 mr-3";
 export const GITHUB_PR_COMMENT_EDITOR_SELECTOR = "flex-nowrap d-inline-block mr-3";
--- src/utils/dom-utils/addPRToHighlights.ts
+++ src/utils/dom-utils/addPRToHighlights.ts
@@ -1,38 +1,26 @@
 import { AddPRToHighlightsButton } from "../../content-scripts/components/AddPRToHighlights/AddPRToHighlightsButton";
-import {
-  GITHUB_LOGGED_IN_USER_USERNAME_SELECTOR,
-  GITHUB_PR_AUTHOR_USERNAME_SELECTOR,
-  GITHUB_PR_COMMENT_HEADER_SELECTOR,
-} from "../../constants";
+import { GITHUB_PR_COMMENT_HEADER_SELECTOR } from "../../constants";
 import { isLoggedIn } from "../checkAuthentication";
 import { isPublicRepository } from "../fetchGithubAPIData";
 
 const injectAddPRToHighlightsButton = async () => {
   if (!(await isLoggedIn())) {
- return;
-}
+    return;
+  }
 
-  const prAuthorUserName = document.getElementsByClassName(
-    GITHUB_PR_AUTHOR_USERNAME_SELECTOR,
-  )[0].textContent;
-  const loggedInUserUserName = document
-    .querySelector(GITHUB_LOGGED_IN_USER_USERNAME_SELECTOR)
-    ?.getAttribute("content");
+  if (!(await isPublicRepository(window.location.href))) {
+    return;
+  }
 
-  if (loggedInUserUserName && prAuthorUserName === loggedInUserUserName) {
-    if (!(await isPublicRepository(window.location.href))) {
- return;
-}
-    const commentFormatRow = document.getElementsByClassName(
+  const commentFormatRow = document.getElementsByClassName(
       GITHUB_PR_COMMENT_HEADER_SELECTOR,
     )[0];
-    const addPRToHighlightsButton = AddPRToHighlightsButton();
+  const addPRToHighlightsButton = AddPRToHighlightsButton();
 
-    commentFormatRow.insertBefore(
+  commentFormatRow.insertBefore(
         addPRToHighlightsButton,
         commentFormatRow.lastElementChild,
       );
-  }
 };
 
 export default injectAddPRToHighlightsButton;
