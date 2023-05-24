5fc32a9f9e540a1b421363cbb1358a89f8f67127
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-05-25 01:06:55 +0300

    feat: add toast and disable post button when fetch

--- src/pages/posthighlight.tsx
+++ src/pages/posthighlight.tsx
@@ -3,10 +3,11 @@ import { FaChevronLeft } from "react-icons/fa";
 import OpenSaucedLogo from "../assets/opensauced-logo.svg";
 import { RouteContext } from "../App";
 import { useAuth } from "../hooks/useAuth";
+import toast, { Toaster } from "react-hot-toast";
 
 
 const PostOnHighlight = () => {
-  const { authToken } = useAuth();
+  const { authToken, user } = useAuth();
   const { setCurrentPage } = useContext(RouteContext);
   const [pageURL, setPageURL] = useState("");
   const [highlightTitle, setHighlightTitle] = useState("");
@@ -15,57 +16,83 @@ const PostOnHighlight = () => {
 
 
   // post highlight function
-  const postHighlight = async () => {
-    const response = await fetch("https://api.opensauced.pizza/v1/user/highlights", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${authToken}`,
-        },
-        method: "POST",
-        body: JSON.stringify({
-          url: pageURL,
-          title: highlightTitle,
-          highlight: highlightContent,
-        }),
+  const postHighlight = () => {
+    enableSendButton(false);
+    const postHighlightAPI = fetch("https://api.opensauced.pizza/v1/user/highlights", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        url: pageURL,
+        title: highlightTitle,
+        highlight: highlightContent,
+      }),
 
     });
-    const data = await response.json();
 
-    console.log(data);
+    toast.promise(postHighlightAPI, {
+      loading: "Loading ...",
+      success: data => {
+        enableSendButton(true);
+        if (!data.ok) {
+          throw new Error(`Statues code ${data.status}`);
+        }
+        return (
+          <span>
+          check the highlight at:
+          <a
+            href={`https://insights.opensauced.pizza/user/${user.user_name}/highlights`}
+            rel="noreferrer"
+            target="_blank"
+          >
+{" "}
+your account
+          </a>
+          </span>
+          );
+      },
+      error: e => {
+        enableSendButton(true);
+        return `Uh oh, there was an error! ${e.message}`;
+      },
+    }).catch(console.error);
   };
 
 
   chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
-    console.log(tabs);
     setPageURL(tabs[0]?.url ?? "");
 
     chrome.tabs.sendMessage(tabs[0].id ?? 0, { type: "get_highlight" }, setHighlightTitle);
-});
+  });
 
 
   return (
     <div className="grid grid-cols-1 divide-y divider-y-center-2 min-w-[320px]">
-        <header className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <button
-              className="rounded-full p-2 bg-slate-700 hover:bg-slate-700/50"
-              onClick={() => {
-                setCurrentPage("home");
-              }}
-            >
-              <FaChevronLeft className="text-osOrange text-white" />
-            </button>
+      <Toaster />
 
-            <img
-              alt="OpenSauced logo"
-              className="w-[100%]"
-              src={OpenSaucedLogo}
-            />
-          </div>
-        </header>
+      <header className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <button
+            className="rounded-full p-2 bg-slate-700 hover:bg-slate-700/50"
+            onClick={() => {
+              setCurrentPage("home");
+            }}
+          >
+            <FaChevronLeft className="text-osOrange text-white" />
+          </button>
 
-        <main className="text-white">
+          <img
+            alt="OpenSauced logo"
+            className="w-[100%]"
+            src={OpenSaucedLogo}
+          />
+        </div>
+      </header>
+
+      <main className="text-white">
 
         <input
           className="p-1.5 rounded-md mb-2 w-full text-black"
@@ -86,11 +113,12 @@ const PostOnHighlight = () => {
 
         <button
           className="inline-block disabled:bg-gray-500 text-black bg-gh-white rounded-md p-2 text-sm font-semibold text-center select-none w-full border hover:shadow-button hover:no-underline"
+          disabled={!isSendButtonEnabled}
           onClick={postHighlight}
         >
-            Post
+          Post
         </button>
-        </main>
+      </main>
     </div>
   );
 };
