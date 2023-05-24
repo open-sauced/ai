b9f6cad865eac49fe6221e3ed8c6d7b367f71ed5
Nanak <nanakbandyopadhyay@hotmail.com>
2023-05-24 07:13:20 +0530

    add getUserPRVelocity utility function

---
+++ src/utils/getUserPRVelocity.ts
@@ -0,0 +1,15 @@
+import { differenceInDays } from "date-fns";
+
+export const getUserPRVelocity = ((prDetails: IUserPR[]) => {
+    const mergedPRs = prDetails.filter((prState: IUserPR) => prState.state.toLowerCase() === "merged");
+
+    const totalDays = mergedPRs.reduce((total: number, pr: IUserPR) => {
+        const daysBetween = differenceInDays(new Date(pr.closed_at), new Date(pr.created_at));
+
+        return (total += daysBetween);
+    }, 0);
+
+    const averageVelocity: number = mergedPRs.length > 0 ? Math.round(totalDays / mergedPRs.length) : 0;
+
+    return averageVelocity;
+});
