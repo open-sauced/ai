e16ae1674b64ad1b0b3ed2c00e416bea03a269dc
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-05-25 17:39:55 +0300

    chore: remove cursor function

--- src/utils/aiprdescription/cursorPositionInsert.ts
+++ src/utils/aiprdescription/cursorPositionInsert.ts
@@ -9,22 +9,3 @@ export const insertTextAtCursor = (textArea: HTMLTextAreaElement, text: string) 
     }
   }, 10);
 };
-
-export const insertAtCursorFromStream = (textArea: HTMLTextAreaElement, stream: ReadableStream<Uint8Array>) => {
-  const reader = stream.getReader();
-  const decoder = new TextDecoder("utf-8");
-
-  while (true) {
-    const { done, value } = await reader.read();
-
-    if (done) {
-      break;
-    } else {
-      const chunk = decoder.decode(value);
-      const [start, end] = [textArea.selectionStart, textArea.selectionEnd];
-
-      textArea.setRangeText(chunk, start, end, "end");
-    }
-  }
-  textArea.setRangeText("\n\n_Generated using [OpenSauced](https://opensauced.ai/)._", textArea.selectionStart, textArea.selectionEnd, "end");
-};
