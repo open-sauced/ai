65baeba00118c18ca6fbb0339d21434897cedf13
Ozgur Sar <ozgursar77@gmail.com>
2023-05-27 14:48:39 +0300

    add tests for urlMatchers

--- test/utils/urlMatcher.test.ts
+++ test/utils/urlMatcher.test.ts
@@ -41,6 +41,55 @@ test('getTwitterUsername', () => {
     expect(getTwitterUsername('twitter.com/123/')).toBe('123')
 })
 
+test('isGithubPullRequestPage', () => {
+  expect(isGithubPullRequestPage('https://www.github.com/')).toBe(false)
+  expect(isGithubPullRequestPage('https://www.github.com/pull')).toBe(false)
+  expect(isGithubPullRequestPage('https://www.github.com/pull/')).toBe(false)
+  expect(isGithubPullRequestPage('https://www.github.com/username/repo/pull/123')).toBe(true)
+  expect(isGithubPullRequestPage('https://www.github.com/username/repo/pull/123/')).toBe(true)
+  expect(isGithubPullRequestPage('www.github.com/username/repo/pull/123')).toBe(true)
+  expect(isGithubPullRequestPage('github.com/username/repo/pull/123/')).toBe(true)
+  expect(isGithubPullRequestPage('abcd')).toBe(false)
+})
 
+test('isGithubProfilePage', () => {
+  expect(isGithubProfilePage('https://www.github.com/')).toBe(false)
+  expect(isGithubProfilePage('https://www.github.com/username')).toBe(true)
+  expect(isGithubProfilePage('https://www.github.com/username/')).toBe(false)
+  expect(isGithubProfilePage('https://www.github.com/username?tab=repositories')).toBe(true)
+  expect(isGithubProfilePage('www.github.com/username')).toBe(true)
+  expect(isGithubProfilePage('github.com/username')).toBe(true)
+  expect(isGithubProfilePage('github.com/username/')).toBe(false)
+  expect(isGithubProfilePage('abcd')).toBe(false)
+})
 
+test('isGithubRepoPage', () => {
+  expect(isGithubRepoPage('https://www.github.com/')).toBe(false)
+  expect(isGithubRepoPage('https://www.github.com/username/')).toBe(false)
+  expect(isGithubRepoPage('https://www.github.com/username/repo')).toBe(true)
+  expect(isGithubRepoPage('https://www.github.com/username/repo/')).toBe(false)
+  expect(isGithubRepoPage('https://www.github.com/username/repo/abcd')).toBe(false)
+  expect(isGithubRepoPage('www.github.com/username/repo')).toBe(true)
+  expect(isGithubRepoPage('github.com/username/repo')).toBe(true)
+  expect(isGithubRepoPage('abcd')).toBe(false)
+})
 
+test('isPullRequestCreatePage', () => {
+  expect(isPullRequestCreatePage('https://www.github.com/')).toBe(false)
+  expect(isPullRequestCreatePage('https://github.com/username/repo/compare/')).toBe(false)
+  expect(isPullRequestCreatePage('https://github.com/username/repo/compare/abcd')).toBe(true)
+  expect(isPullRequestCreatePage('https://github.com/username/repo/compare/abcd/')).toBe(true)
+  expect(isPullRequestCreatePage('www.github.com/username/repo/compare/abcd/')).toBe(true)
+  expect(isPullRequestCreatePage('github.com/username/repo/compare/abcd/')).toBe(true)
+  expect(isPullRequestCreatePage('abcd')).toBe(false)
+})
+
+test('isPullRequestFilesChangedPage', () => {
+  expect(isPullRequestFilesChangedPage('https://www.github.com/')).toBe(false)
+  expect(isPullRequestFilesChangedPage('https://github.com/username/repo/pull/123/files')).toBe(true)
+  expect(isPullRequestFilesChangedPage('https://github.com/username/repo/pull/123/files/')).toBe(true)
+  expect(isPullRequestFilesChangedPage('www.github.com/username/repo/pull/123/files')).toBe(true)
+  expect(isPullRequestFilesChangedPage('github.com/username/repo/pull/123/files/')).toBe(true)
+  expect(isPullRequestFilesChangedPage('github.com/username/repo/pull/123/fil')).toBe(false)
+  expect(isPullRequestFilesChangedPage('abcd')).toBe(false)
+})
