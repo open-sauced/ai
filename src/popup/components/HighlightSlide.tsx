3755e66fe3254f4c1038c1363745512ba0aea596
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-06-10 21:09:21 +0530

    uniform heights

--- src/popup/components/HighlightSlide.tsx
+++ src/popup/components/HighlightSlide.tsx
@@ -11,11 +11,23 @@ interface HighlightSlideProps {
 export const HighlightSlide = ({ highlight }: HighlightSlideProps) => {
     const { url, title, login, highlight: highlightText } = highlight;
 
+    const openGraphSearchParameter = url.split("/").slice(3)
+        .join("/");
+    const openGraphUrl = `https://opengraph.githubassets.com/1/${openGraphSearchParameter}`;
+
     return (
-        <div className="border border-white/40 rounded-sm p-3 mt-2">
-            <h3 className="text-base font-medium">
+        <div className="border border-white/40 rounded-md p-3 mt-2 bg-white">
+            {/* fixed height, content ellipsis */}
+            <h3 className="
+                text-base font-medium
+                overflow-hidden
+                line-clamp-2
+                h-6
+                leading-5
+            "
+            >
                 <a
-                    className="text-blue-500 hover:text-blue-700 underline cursor-pointer"
+                    className="text-slate-800 cursor-pointer"
                     href={url}
                     rel="noopener noreferrer"
                     target="_blank"
@@ -24,25 +36,34 @@ export const HighlightSlide = ({ highlight }: HighlightSlideProps) => {
                 </a>
             </h3>
 
-
             <div className="flex items-center">
-                <div className="mr-2">Author:</div>
+                <span className="mr-2 text-slate-500">Author:</span>
 
                 <a
-                    className="text-blue-500 hover:text-blue-700 underline cursor-pointer"
+                    className="text-orange cursor-pointer"
                     href={`https://insights.opensauced.pizza/user/${login}`}
                     rel="noopener noreferrer"
                     target="_blank"
-
-
                 >
                     {login}
                 </a>
             </div>
 
-            <p className="py-2">
+            <p className="
+                py-2 text-sm text-slate-500
+                overflow-hidden
+                line-clamp-3
+                h-16
+                leading-5
+            "
+            >
                 {highlightText}
             </p>
+
+            <img
+                alt="OpenGraph"
+                src={openGraphUrl}
+            />
 
         </div>);
 };
