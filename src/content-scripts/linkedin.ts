109547cc9c863caf715384bf01c5c5695b545d5a
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-06-12 01:14:49 +0300

    chore: remove content-script linkedin

--- src/content-scripts/linkedin.ts
+++
@@ -1,5 +0,0 @@
-console.log("post_linkedin INJECT");
-const inputFields = document.querySelectorAll(".artdeco-text-input--input");
-
-inputFields[0].value = "Project Name";
-inputFields[1].value = "Project Description";
