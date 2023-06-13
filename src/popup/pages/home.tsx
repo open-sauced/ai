03926540cc0ae2203d00bda69d65a40e147e1d46
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-06-14 03:04:54 +0530

    feat: carousel design for the highlights feed (#173)
    * feat: carousel design for the highlights feed
    
    * uniform heights
    
    * add a11y navigation pagination and remove highlights feed separate link
    
    * lint and update package

--- src/popup/pages/home.tsx
+++ src/popup/pages/home.tsx
@@ -1,12 +1,13 @@
 import {
-    HiArrowLeft,
-    HiArrowRight,
     HiArrowTopRightOnSquare,
     HiOutlineQuestionMarkCircle,
     HiPencil,
     HiUserCircle,
 } from "react-icons/hi2";
 import { FiSettings } from "react-icons/fi";
+import { Navigation, Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
 import OpenSaucedLogo from "../../assets/opensauced-logo.svg";
 import { useAuth } from "../../hooks/useAuth";
 import { useOpensaucedUserCheck } from "../../hooks/useOpensaucedUserCheck";
@@ -22,13 +23,13 @@ import Settings from "./settings";
 import { OPEN_SAUCED_INSIGHTS_DOMAIN } from "../../constants";
 import type { Highlight } from "../../ts/types";
 import { useIsGithubPRPageCheck } from "../../hooks/useGithubPRPageCheck";
+import { HighlightSlide } from "../components/HighlightSlide";
 
 const Home = () => {
     const { user } = useAuth();
     const { currentTabIsOpensaucedUser, checkedUser } = useOpensaucedUserCheck();
     const { isGithubPRPage, prUrl, prTitle } = useIsGithubPRPageCheck();
     const [highlights, setHighlights] = useState<Highlight[]>([]);
-    const [currentPage, setCurrentPage] = useState(0);
 
     useEffect(() => {
         const fetchHighlights = async () => {
@@ -48,14 +49,6 @@ const Home = () => {
 
         void fetchHighlights();
     }, []);
-
-    const handlePrevious = () => {
-        setCurrentPage(prevPage => prevPage - 1);
-    };
-
-    const handleNext = () => {
-        setCurrentPage(prevPage => prevPage + 1);
-    };
 
     return (
         <div className="p-4 bg-slate-800">
@@ -87,76 +80,40 @@ const Home = () => {
                 </header>
 
                 <main className="main-content">
-                    <h3 className="text font-medium text-base leading-10">Latest Highlight:</h3>
+                    <a
+                        className="flex items-center text-white hover:text-orange no-underline gap-2 w-full font-medium text-lg leading-10"
+                        href={`https://${OPEN_SAUCED_INSIGHTS_DOMAIN}/feed`}
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                            Highlights feed
+                        <HiArrowTopRightOnSquare />
+                    </a>
 
                     {highlights.length > 0 && (
+                        <Swiper
+                            navigation
+                            modules={[Navigation, Pagination, A11y]}
+                            pagination={{ clickable: true }}
+                            slidesPerView={1}
+                            spaceBetween={50}
+                        >
+                            {
+                                highlights.map((highlight, index) => (
+                                    <SwiperSlide key={index}>
+                                        <HighlightSlide
+                                            highlight={highlight}
+                                        />
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                    )}
 
-                        <div className="border border-white/40 rounded-sm p-3 mt-2">
-                            <h3 className="text-base font-medium">
-                                <a
-                                    className="text-blue-500 hover:text-blue-700 underline cursor-pointer"
-                                    href={highlights[currentPage]?.url}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                >
-                                    {highlights[currentPage]?.title}
-                                </a>
-                            </h3>
-
-
-                            <div className="flex items-center">
-                                <div className="mr-2">Author:</div>
-
-                                <a
-                                    className="text-blue-500 hover:text-blue-700 underline cursor-pointer"
-                                    href={`https://insights.opensauced.pizza/user/${highlights[currentPage]?.login}`}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-
-
-                                >
-                                    {highlights[currentPage]?.login}
-                                </a>
-                            </div>
-
-                            <p className="py-2">
-                                {highlights[currentPage]?.highlight}
-                            </p>
-
-                            <div className="flex justify-center">
-                                <div className="flex justify-center mt-4">
-                                    <button
-                                        className="px-4 py-2 rounded-md bg-blue-500 text-white disabled:opacity-50"
-                                        disabled={currentPage === 0}
-                                        onClick={handlePrevious}
-                                    >
-                                        <HiArrowLeft />
-                                    </button>
-
-                                    <button
-                                        className="px-4 py-2 rounded-md bg-blue-500 text-white disabled:opacity-50 ml-4"
-                                        disabled={currentPage === highlights.length - 1}
-                                        onClick={handleNext}
-                                    >
-                                        <HiArrowRight />
-                                    </button>
-                                </div>
-                            </div>
-                        </div>)}
 
                     <h3 className="text font-medium text-base leading-10">Tools:</h3>
 
                     <div className="tools flex flex-col gap-2">
-                        <a
-                            className="flex items-center bg-slate-700 hover:bg-slate-700/70 text-white hover:text-orange no-underline gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
-                            href={`https://${OPEN_SAUCED_INSIGHTS_DOMAIN}/feed`}
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <HiArrowTopRightOnSquare />
-                            Highlights feed
-                        </a>
-
                         <a
                             className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
                             href={`https://${OPEN_SAUCED_INSIGHTS_DOMAIN}/feed`}
