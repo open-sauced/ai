4e6fe6da03a66615bc105dc73b85941f25cc038a
TheLazron <kaushik.rohan31@gmail.com>
2023-06-20 04:07:31 +0530

    fixed typos for tests

--- test/utils/getUserPRVelocity.test.ts
+++ test/utils/getUserPRVelocity.test.ts
@@ -3,7 +3,7 @@ import { getUserPRVelocity } from "../../src/utils/getUserPRVelocity";
 import { IUserPR } from "../../src/ts/types";
 
 describe("getUserPRVelocity", () => {
-  it("should return consider all enteries to return averageVelocity", () => {
+  it("should consider all PRs to calculate PRVelocity", () => {
     const prDetails = [
       { state: "merged", created_at: "2022-12-24", closed_at: "2023-01-15" },
       { state: "merged", created_at: "2023-02-18", closed_at: "2023-02-19" },
@@ -13,7 +13,7 @@ describe("getUserPRVelocity", () => {
     expect(averageVelocity).toBe(10);
   });
 
-  it("should only consider closed entries it has a cloesd_at property", () => {
+  it("should only consider closed PRs with a closed_at field", () => {
     const prDetails = [
       { state: "open", created_at: "2023-06-01", closed_at: "2023-06-03" },
       { state: "open", created_at: "2023-06-05", closed_at: "" },
@@ -22,7 +22,7 @@ describe("getUserPRVelocity", () => {
     expect(averageVelocity).toBe(0);
   });
 
-  it("should return 0 if there are not enteries ", () => {
+  it("should return 0 if there are no enteries ", () => {
     const prDetails = [];
     const averageVelocity = getUserPRVelocity(prDetails);
     expect(averageVelocity).toBe(0);
