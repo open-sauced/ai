7a980c82d1056cd1f798c39e51ba9fbc4d205ad7
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-06-21 23:53:07 +0530

    fix: separate vite and vitest configs (#193)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json" assert { type: "json" };
+import * as manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    environment: "jsdom",
-  },
+  plugins: [react(), crx({ manifest } as any)],
 });
