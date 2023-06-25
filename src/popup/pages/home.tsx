9ecdced615d3ead3c20b07145045e22ae5e35d9f
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-06-26 00:26:36 +0530

    ran "npm run format" (i hate lint)

--- src/popup/pages/home.tsx
+++ src/popup/pages/home.tsx
@@ -151,7 +151,7 @@ const Home = () => {
                             <button
                                 className="flex items-center bg-slate-700 hover:bg-slate-700/70 hover:text-orange text-white gap-2 p-1.5 px-3 w-full rounded-sm font-medium text-sm"
                                 onClick={() => {
-                                    function populateDataToLinkedIn(data: any) {
+                                    function populateDataToLinkedIn (data: any) {
                                         const inputFields: NodeListOf<HTMLInputElement> = document.querySelectorAll(".artdeco-text-input--input");
 
                                         if (inputFields.length === 0) {
