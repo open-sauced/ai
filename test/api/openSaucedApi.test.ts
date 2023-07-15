13d69b5a1810c2fe8f93bdc07fea30110391ccee
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-07-15 13:21:28 +0530

    add test for repos endpoint

--- test/api/openSaucedApi.test.ts
+++ test/api/openSaucedApi.test.ts
@@ -2,11 +2,10 @@ import { describe, it, expect } from "vitest"
 
 import {
     OPEN_SAUCED_USERS_ENDPOINT,
-    OPEN_SAUCED_SESSION_ENDPOINT,
     OPEN_SAUCED_REPOS_ENDPOINT,
-    OPEN_SAUCED_USER_INSIGHTS_ENDPOINT,
 } from "../../src/constants";
 import { UserResponse } from "./types/userResponse";
+import { RepoResponse } from "./types/repoResponse"
 
 describe("openSaucedUserEndpoint", () => {
     it("should return a user object", async () => {
@@ -17,6 +16,19 @@ describe("openSaucedUserEndpoint", () => {
 
     it("should return a 404 error for a user that does not exist", async () => {
         const response = await fetch(`${OPEN_SAUCED_USERS_ENDPOINT}/1`);
+        expect(response.status).toBe(404);
+    })
+})
+
+describe("openSaucedRepoEndpoint", () => {
+    it("should return a repository object", async () => {
+        const response = await fetch(`${OPEN_SAUCED_REPOS_ENDPOINT}/71359796`);
+        const data = await response.json() as RepoResponse;
+        expect(data.full_name).toBe("open-sauced/open-sauced");
+    })
+
+    it("should return a 404 error for a repo that does not exist", async () => {
+        const response = await fetch(`${OPEN_SAUCED_REPOS_ENDPOINT}/0`);
         expect(response.status).toBe(404);
     })
 })
