c738f6a51a27ec32806f02a05711be97299c2daa
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-07-21 18:33:18 +0530

    add spinner for indexign

--- src/content-scripts/components/ChatDialog/ChatDialog.ts
+++ src/content-scripts/components/ChatDialog/ChatDialog.ts
@@ -44,7 +44,7 @@ const handleChatDialogClick = (ownerName: string, repoName: string) => {
                 us first. Click below to check if this repository is indexed and start the conversation!</p>
                 <button
                     class="bg-orange no-underline border-none rounded-md text-white font-bold py-2 px-4 mt-4 cursor-pointer
-                    bg-gradient-to-r from-[#e67e22] to-[#d35400]"
+                    bg-gradient-to-r from-[#e67e22] to-[#d35400]" id="chat-dialog-body-start-conversation-button"
                 >
                     Start the conversation
                 </button>
@@ -60,6 +60,30 @@ const handleChatDialogClick = (ownerName: string, repoName: string) => {
 
         document.getElementById("chat-dialog-header-left-close")?.addEventListener("click", () => {
             document.getElementById("chat-dialog")?.remove();
+        });
+
+        document.getElementById("chat-dialog-body-start-conversation-button")?.addEventListener("click", () => {
+            document.getElementById("chat-dialog-body-start-conversation")?.remove();
+            const chatDialogBody = createHtmlElement("div", {
+                id: "chat-dialog-body-loading-spinner",
+                className: "flex flex-col justify-center items-center w-full h-full",
+                innerHTML: `
+                    <div class="text-2xl font-bold text-slate-700">RepoQuery</div>
+                    <p class="text-sm text-gray-500 w-5/6 text-center mt-4">We are indexing ${ownerName}/${repoName}. This may take a while.</p>
+                    <div class="mt-4">
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-orange" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0
+                                004 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                            </path>
+                        </svg>
+                    </div>
+                `,
+            });
+
+            document.getElementById("chat-dialog-body")?.appendChild(chatDialogBody);
         });
     }
 };
