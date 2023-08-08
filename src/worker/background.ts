765cf0a0f0e248e4a4bccf546cdaa710b4a7be13
Nick Taylor <nick@iamdeveloper.com>
2023-08-07 22:05:24 -0400

    fix: changed chrome global to browser to support Firefox

--- src/worker/background.ts
+++ src/worker/background.ts
@@ -1,3 +1,13 @@
 import { setDefaultDescriptionConfig } from "../utils/ai-utils/descriptionconfig";
 
-chrome.runtime.onInstalled.addListener(setDefaultDescriptionConfig);
+// not sure why the linter/prettier are fighting between two and four spaces here
+declare global {
+  interface Window {
+    browser: typeof chrome;
+  }
+
+  // eslint-disable-next-line no-var
+  var browser: typeof chrome;
+}
+
+browser.runtime.onInstalled.addListener(setDefaultDescriptionConfig);
