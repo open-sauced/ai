011f54011fc90a6c0b7c1d9ed79c010cc484aa38
Anush008 <anushshetty90@gmail.com>
2023-08-09 11:03:24 +0530

    fix: public repo check

--- src/hooks/useGetGitHubPageInfo.ts
+++ src/hooks/useGetGitHubPageInfo.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { isGithubPullRequestPage, isGithubRepoPage } from "../utils/urlMatchers";
+import { isPublicRepository } from "../utils/fetchGithubAPIData";
 
 interface GitHubPageInfo {
     pageUrl: string;
@@ -11,13 +12,13 @@ export const usGetGitHubPageInfo = () => {
     const [GithubPage, setGithubPage] = useState<GitHubPageInfo>({ pageUrl: "", pageTitle: "", type: "unknown" });
 
     useEffect(() => {
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.query({ active: true, currentWindow: true }, async tabs => {
             if (tabs.length > 0) {
                 const tab = tabs[0];
 
                 if (isGithubPullRequestPage(tab.url!)) {
                     setGithubPage({ pageUrl: tab.url!, pageTitle: tab.title!.split("by")[0].trim(), type: "PR" });
-                } else if (isGithubRepoPage(tab.url!)) {
+                } else if (isGithubRepoPage(tab.url!) && (await isPublicRepository(tab.url!))) {
                     setGithubPage({ pageUrl: tab.url!, pageTitle: "", type: "REPO" });
                 }
             }
