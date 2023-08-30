27e67fd22c2479606214c14157993edc1926c713
Marcus Chan <marcuscchan@gmail.com>
2023-08-30 15:48:17 +0800

    Change spacing to 4

--- e2e/popupFixture.ts
+++ e2e/popupFixture.ts
@@ -1,31 +1,30 @@
-import { test as base, chromium, type BrowserContext } from '@playwright/test';
-import path from 'path';
+import { test as base, chromium, type BrowserContext } from "@playwright/test";
+import path from "path";
 
 export const test = base.extend<{
-  context: BrowserContext;
-  extensionId: string;
+    context: BrowserContext;
+    extensionId: string;
 }>({
-  context: async ({ }, use) => {
-    const pathToExtension = path.join(__dirname, '../dist');
-    const context = await chromium.launchPersistentContext('', {
-      headless: false,
-      args: [
-        `--headless=new`,
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`,
-      ],
-    });
-    await use(context);
-    await context.close();
-  },
-  extensionId: async ({ context }, use) => {
-    let [background] = context.serviceWorkers();
-    if (!background)
-      background = await context.waitForEvent('serviceworker');
+    context: async ({}, use) => {
+        const pathToExtension = path.join(__dirname, "../dist");
+        const context = await chromium.launchPersistentContext("", {
+            headless: false,
+            args: [
+                `--headless=new`,
+                `--disable-extensions-except=${pathToExtension}`,
+                `--load-extension=${pathToExtension}`,
+            ],
+        });
+        await use(context);
+        await context.close();
+    },
+    extensionId: async ({ context }, use) => {
+        let [background] = context.serviceWorkers();
+        if (!background) background = await context.waitForEvent("serviceworker");
 
-    const extensionId = background.url().split('/')[2];
-    await use(extensionId);
-  },
+        const extensionId = background.url().split("/")[2];
+        await use(extensionId);
+    },
 });
 
 export const expect = test.expect;
