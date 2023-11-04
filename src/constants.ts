0969cc9458efde0061a95e14d9cdaac4ee2c1930
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-11-04 19:44:31 +0300

    chore: renaming and removing extra code

--- src/constants.ts
+++ src/constants.ts
@@ -31,7 +31,6 @@ export const OPEN_SAUCED_EMOJIS_ENDPOINT = `${OPEN_SAUCED_API_ENDPOINT}/emojis`;
 // Content-scripts selectors
 export const GITHUB_PROFILE_MENU_SELECTOR = ".p-nickname.vcard-username.d-block";
 export const GITHUB_PROFILE_EDIT_MENU_SELECTOR = "button.js-profile-editable-edit-button";
-export const GITHUB_PROFILE_USER_PROFILE_EDITABLE_AREA_SELECTOR = ".btn-block.js-profile-editable-edit-button";
 export const GITHUB_PR_COMMENT_HEADER_SELECTOR = "timeline-comment-header clearfix d-flex";
 export const GITHUB_NEW_PR_COMMENT_EDITOR_SELECTOR = "flex-nowrap d-none d-md-inline-block mr-md-0 mr-3";
 export const GITHUB_PR_COMMENT_EDITOR_SELECTOR = "flex-nowrap d-inline-block mr-3";
--- src/utils/dom-utils/viewOnOpenSauced.ts
+++ src/utils/dom-utils/viewOnOpenSauced.ts
@@ -1,4 +1,3 @@
-import { GITHUB_PROFILE_USER_PROFILE_EDITABLE_AREA_SELECTOR } from "../../constants";
 import { ViewOnOpenSaucedButton } from "../../content-scripts/components/ViewOnOpenSaucedButton/ViewOnOpenSaucedButton";
 
 const injectViewOnOpenSaucedButton = (username: string) => {
@@ -8,17 +7,14 @@ const injectViewOnOpenSaucedButton = (username: string) => {
 
     const viewOnOpenSaucedButton = ViewOnOpenSaucedButton(username);
 
-    const userEditableArea = document.getElementsByClassName(
-        GITHUB_PROFILE_USER_PROFILE_EDITABLE_AREA_SELECTOR,
-    );
-    const editableAreaElement = userEditableArea[0];
     const editProfileButtonSelector = ".btn-block.js-profile-editable-edit-button"
     const editProfileButton = document.querySelector(editProfileButtonSelector);
+
     if (editProfileButton) {
         editProfileButton.parentNode?.insertBefore(viewOnOpenSaucedButton, editProfileButton)
     } else {
-        const ButtonArea = document.querySelector(".js-user-profile-follow-button")?.closest(".flex-order-1.flex-md-order-none")
-        ButtonArea?.insertBefore(viewOnOpenSaucedButton, ButtonArea?.firstChild);
+        const callToActionButtonsArea = document.querySelector(".js-user-profile-follow-button")?.closest(".flex-order-1.flex-md-order-none")
+        callToActionButtonsArea?.insertBefore(viewOnOpenSaucedButton, callToActionButtonsArea?.firstChild);
     }
 };
 
