7ab9847ee33350709e70facf5465fcd6c90c63d9
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2024-08-15 20:12:59 -0700

    fixes test

--- test/api/openSaucedApi.test.ts
+++ test/api/openSaucedApi.test.ts
@@ -14,9 +14,9 @@ describe("openSaucedUserEndpoint", () => {
         expect(data.login).toBe("bdougie");
     })
 
-    it("should return a 404 error for a user that does not exist", async () => {
-        const response = await fetch(`${OPEN_SAUCED_USERS_ENDPOINT}/1`);
-        expect(response.status).toBe(404);
+    it("should not return a 200 error for a user that does not exist", async () => {
+        const response = await fetch(`${OPEN_SAUCED_USERS_ENDPOINT}/foolala`);
+        expect(response.status).not.toBe(200);
     })
 })
 
